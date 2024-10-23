997bdab935c200fec2fece65557296934ccb1085
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2024-10-23 23:17:58 +0100

    Update student_operations.h

--- student_operations.h
+++ student_operations.h
@@ -4,43 +4,25 @@
 #include "student_records.h"
 
 /**
- * searchAndDisplayStudent - Searches for a student by roll number and displays their information.
- *
- * @students: An array of Student pointers.
- * @numStudents: The number of students in the array.
- */
+ * searchAndDisplayStudent - Searches for a student by roll number and displays their information.
+ *
+ * @students: An array of Student pointers.
+ * @numStudents: The number of students in the array.
+ */
 void searchAndDisplayStudent(Student **students, int numStudents);
 
 /**
- * modifyStudent - Modifies the information of a student.
- *
- * @students: An array of Student pointers.
- * @numStudents: The number of students in the array.
- */
-
-void modifyStudent(Student **students, int numStudents);
+ * displayStudentInfo - Displays the information of a student.
+ *
+ * @student: A pointer to the student to display.
+ */
+void displayStudentInfo(const Student *student);
 
 /**
- * deleteStudent - Deletes a student from the array.
- *
- * @students: An array of Student pointers.
- * @numStudents: A pointer to the number of students in the array.
- * @index: The index of the student to delete.
- */
-void deleteStudent(Student **students, int *numStudents, int index);
-
-/**
- * displayStudentInfo - Displays the information of a student.
- *
- * @student: A pointer to the student to display.
- */
-void displayStudentInfo(Student *student);
-
-/**
- * modifyStudentInfo - Modifies the information of a student.
- *
- * @student: A pointer to the student to modify.
- */
+ * modifyStudentInfo - Modifies the information of a student.
+ *
+ * @student: A pointer to the student to modify.
+ */
 void modifyStudentInfo(Student *student);
 
 #endif /* STUDENT_OPERATIONS_H */
